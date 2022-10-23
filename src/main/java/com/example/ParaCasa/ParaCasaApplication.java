7c9202f5e04afaa49c30d2a1ed742b6f1e236409
Gerard Siles <67659282+gerardsiles@users.noreply.github.com>
2022-10-23 20:23:46 +0000

    initial commit

---
+++ src/main/java/com/example/ParaCasa/ParaCasaApplication.java
@@ -0,0 +1,11 @@
+package com.example.ParaCasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaCasaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ParaCasaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/ParaCasa/ParaCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParaCasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

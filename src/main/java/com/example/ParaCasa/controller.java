c1888f6b9a0fce7d0012338088e038d1503bf8f1
Gerard Siles <gerardsiles@gmail.com>
2022-10-23 20:12:24 +0000

    initial commit

--- src/main/java/com/example/ParaCasa/controller.java
+++ src/main/java/com/example/ParaCasa/controller.java
@@ -3,6 +3,7 @@ package com.example.ParaCasa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 
+
 @Controller
 public class controller {
 	@GetMapping("/")
